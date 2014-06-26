757e7f4444f2a2d05fc69f88eefdd9e1634dd9f7
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-25 17:22:40 -0700

    Refactor of the page, so the favicon and the utility links are created by content providers.

---
+++ gs/content/layout/interfaces.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from zope.viewlet.interfaces import IViewletManager
+
+
+class IFavicon(IViewletManager):
+    '''A viewlet manager for the favicons'''
+
+
+class IUtilityLinks(IViewletManager):
+    '''A viewlet manager for the utility links'''
