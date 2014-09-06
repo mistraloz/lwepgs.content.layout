6d2041cd00fe4ca084950b8b639a86c4f5a526f1
Alice Murphy <ajr@posteo.de>
2014-09-06 13:29:43 -0700

    Specified i18n translation strings for log in/out links. Added .pot file and German .po file. Ignored .mo files.

--- gs/content/layout/__init__.py
+++ gs/content/layout/__init__.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
-# This space intentionally left blank
+from zope.i18nmessageid import MessageFactory
+from AccessControl import ModuleSecurityInfo
+
+GSMessageFactory = MessageFactory('gs.content.layout')
+
+ModuleSecurityInfo('gs.content.layout').declarePublic('_')
